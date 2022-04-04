d45452f6f37dabb70edd0f38f303b9dd4fe332ee
prasiso <kelwinsimoesdesouza@gmail.com>
2022-04-04 16:13:26 -0300

    fix: turma listagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListagemComponent } from '../app/page/escola/listagem/listagem.component';
 import { FormComponent } from './page/escola/form/form.component';
 
+//Turma
+import { TurmaListagemComponent } from '../app/page/turma/turma-listagem/turma-listagem.component';
+// import { FormComponent } from './page/escola/form/form.component';
+
 const routes: Routes = [
   //Escola
   { path: 'escola', component: ListagemComponent },
   { path: 'escola/:id', component: FormComponent },
+
+  //Turma
+  { path: 'turma', component: TurmaListagemComponent },
+  // { path: 'turma/:id', component: FormComponent },
 ];
 
 @NgModule({
--- src/app/page/turma/turma-listagem/turma-listagem.component.ts
+++ src/app/page/turma/turma-listagem/turma-listagem.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-turma-listagem',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurmaListagemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal, private route: Router,) { }
+  closeResult: string = '';
+  rows: any[] = [];
+  dados: any = {};
+  escola: number = 1;
+  bread: any[] = [];
+  columns: any[] = [];
+  buttons: any[] = [];
 
+  verForm(log: any, content: any = undefined ) { 
+    this.dados = log
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size:'lg' })
+  }
+  deleteForm(log: any) {
+    console.log(log);
+  }
+  editarForm(log: any) {
+    this.route.navigate([`/turma-formulario/${log.id}`])
+
+  }
+  variaveis(){
+    this.buttons = [
+      {
+        text: 'Adicionar',
+        color: 'success',
+        link: '/turma-formulario/0',
+      },
+    ];
+    this.bread = [
+      {
+        text: 'Turma',
+      },
+      {
+        text: 'Listagem',
+        color: 'gray',
+      },
+    ];
+    this.rows = [
+      {
+        id: 1,
+        nome: 'Kelwin de Souza',
+      },
+      {
+        id: 2,
+        nome: 'Patricia de Souza',
+      },
+    ];
+    this.columns = [
+      {
+        label: 'Identificador',
+        key: 'id',
+      },
+      {
+        label: 'Nome',
+        key: 'nome',
+      },
+    ];
+  }
   ngOnInit(): void {
+    this.variaveis()
   }
 
 }

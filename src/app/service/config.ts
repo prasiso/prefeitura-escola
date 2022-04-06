73eec71ec15e525043a4e91339e49c27fbb8898f
prasiso <kelwinsimoesdesouza@gmail.com>
2022-04-06 13:33:54 -0300

    fix: novos links

--- src/app/service/config.ts
+++ src/app/service/config.ts
@@ -1,3 +1,5 @@
+import {environment } from '../../environments/environment'
 export default {
-  url:'http://localhost:3000/'
+  // url:environment.url ||'http://localhost:3000/'
+  url:environment.url ||'https://prefeitura-escola-api.herokuapp.com/'
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url:'https://prefeitura-escola-api.herokuapp.com/'
 };
